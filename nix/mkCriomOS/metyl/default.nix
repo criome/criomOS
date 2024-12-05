bb14596bb2697c5385d549d68ed7baaa98bd69b9
li <li@maisiliym.criome.me>
2024-12-05 15:16:48 +0100

    Revert "added keyd"
    This reverts commit 62920ab2a88437f9377a6ac3ef278fcc221dcc39.

--- nix/mkCriomOS/metyl/default.nix
+++ nix/mkCriomOS/metyl/default.nix
@@ -139,21 +139,6 @@ in
       };
     };
 
-    keyd = {
-      enable = true;
-      keyboards = {
-        default = {
-          ids = [ "0001:0001" ];
-          settings = {
-            main = {
-              capslock = "overload(control, esc)";
-              esc = "capslock";
-            };
-          };
-        };
-      };
-    };
-
     localtimed = { enable = saizAtList.min; };
 
     printing = {
