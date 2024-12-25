a5fc2857148f2e7faa1545e1fb95f4e143030b34
li <li@maisiliym.criome.me>
2024-12-26 00:05:50 +0100

    removed(broken(chromium.extension))

--- nix/homeModule/max/default.nix
+++ nix/homeModule/max/default.nix
@@ -37,10 +37,7 @@ kor.mkIf saizAtList.max {
       enable = true;
       extensions = [
         { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
-        {
-          id = "dcpihecpambacapedldabdbpakmachpb";
-          updateUrl = "https://raw.githubusercontent.com/iamadamdev/bypass-paywalls-chrome/master/updates.xml";
-        }
+        { id = "dbepggeogbaibhgnhhndojpepiihcmeb"; } # vimium
       ];
     };
 
