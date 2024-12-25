2ffa6a180ec40bace6fc02ac292d17bb0c114680
li <li@maisiliym.criome.me>
2024-12-25 21:59:42 +0100

    fixed(broken(hyprlandSettings))

--- nix/homeModule/min/hyprland.nix
+++ nix/homeModule/min/hyprland.nix
@@ -64,10 +64,6 @@ in
 
     decoration {
       rounding = 0
-      drop_shadow = yes
-      shadow_range = 4
-      shadow_render_power = 3
-      col.shadow = rgba(1a1a1aee)
       fullscreen_opacity = 0.9999999
       dim_strength = 0.25
     }
@@ -193,9 +189,8 @@ in
           command = lockScreen;
         }
       ];
-      events =
-        [
-        ];
+      events = [
+      ];
     };
   };
 }
